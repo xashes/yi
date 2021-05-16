5393a2eee004a81e18b75c13adea1be17b93ee3c
xashes <xashes@outlook.com>
2021-05-16 19:15:05 +0800

    data path and encoding

--- yi/data.py
+++ yi/data.py
@@ -3,7 +3,8 @@
 import yaml
 import os
 
-gua64_file = os.path.expanduser("~/data/gua64.yaml")
+# gua64_file = os.path.expanduser("~/data/gua64.yaml")
+gua64_file = './data/gua64.yaml'
 
-with open(gua64_file) as fh:
+with open(gua64_file, encoding='utf-8') as fh:
     gua64_data = yaml.load(fh, Loader=yaml.FullLoader)
